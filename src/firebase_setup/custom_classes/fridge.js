76e55a07d8f6839d6f7116390278dc909ae833d7
jennifer2430 <jchen243028@gmail.com>
2023-02-11 12:17:25 -0800

    Set up basic data model for fridge collection

---
+++ src/firebase_setup/custom_classes/fridge.js
@@ -0,0 +1,28 @@
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+import db from '../firebase'
+
+class Fridge {
+    constructor(groupName, users, groceries) {
+        this.groupName = groupName;
+        this.users = users;
+        this.groceries = groceries;
+    }
+}
+
+const FridgeConverter = {
+    toFirestore: (fridge) => {
+        return {
+            groupName: fridge.groupName,
+            users: fridge.users,
+            groceries: fridge.groceries
+        };
+    },
+    fromFirestore: (snapshot, options) => {
+        const data = snapshot.data(options);
+        return new Fridge(data.groupName, data.users, data.groceries)
+    }
+};
+
+export { Fridge, FridgeConverter }
+
+
