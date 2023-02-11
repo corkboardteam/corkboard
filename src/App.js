d60434c389ecfdd143e11a2b1345d656a3adbe84
Sandra Lu <classslu@lnxsrv15.seas.ucla.edu>
2023-02-11 10:27:26 -0800

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 
 function App() {
   return (
+    <body>
+      Content goes here.
+      </body>
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +23,7 @@ function App() {
         </a>
       </header>
     </div>
+    */
   );
 }
 
