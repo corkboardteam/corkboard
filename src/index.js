eedda17951a12dc211f65c90db59f4ac3df8049b
jennifer2430 <jchen243028@gmail.com>
2023-02-17 11:07:04 -0800

    Installed react-router-dom
    * index.js: Added two routes: /, which now renders to the App component, and /test, which renders to the GroceryList component in development

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './frontend/styles/index.css';
 import App from './App';
+import TestClass from './backend/custom_classes/test_custom_class/testclass';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/test",
+    element: <TestClass />,
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
