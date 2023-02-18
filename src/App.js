9d38e460d1e0c3ba356861c38c89f279af32e83b
Alejandro B <alejandrojb@g.ucla.edu>
2023-02-17 16:50:58 -0800

    Updated Login.jsx and App.js

--- src/App.js
+++ src/App.js
@@ -8,21 +8,15 @@ import { AuthProvider } from './backend/custom_classes/authContext'
 
 function App() {
   return (
-
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <h1 className='text-center text-3xl font-bold mt-8'>
-        Corkboard
-      </h1>
-      <Container style={{ maxWidth: "400px", marginTop: "2rem" }}>
-        <AuthProvider>
-          <Routes>
-            <Route exact path = '/' element={<Dashboard />} />
-            <Route path = '/login' element={<Login />} />
-            <Route path = '/signup' element={<Signup />} />
-          </Routes>
-        </AuthProvider>
-      </Container>
-    </div>
+    <Container style={{ maxWidth: "600px", marginTop: "2rem" }}>
+      <AuthProvider>
+        <Routes>
+          <Route path = '/' element={<Login />} />
+          <Route path = '/dashboard' element={<Dashboard />} />
+          <Route path = '/signup' element={<Signup />} />
+        </Routes>
+      </AuthProvider>
+    </Container>
   );
 }
 
