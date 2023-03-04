cac318f3fe3b8adaa55a67f32231a9ad8e345ab5
jfan981 <108176614+jfan981@users.noreply.github.com>
2023-03-04 05:02:27 -0800

    Changed axios link, saving before switching branches

--- src/backend/custom_classes/calendar.js
+++ src/backend/custom_classes/calendar.js
@@ -1,39 +1,44 @@
 import axios from 'axios';
 
+class Calendar {
+    constructor(calendarId) {
+        this.calendarId = calendarId;
+    }
+}
+
 class Event {
-  constructor(calendarId, eventId) {
-    this.calendarId = calendarId;
-    this.eventId = eventId;
-  }
+    constructor(eventId) {
+        this.eventId = eventId;
+    }
 }
 
 export function addEventToPrivateCalendar(eventData, accessToken) {
-  return axios.post('https://www.googleapis.com/calendar/v3/calendars/primary/events', eventData, {
-    headers: {
-      'Authorization': 'Bearer ' + accessToken,
-      'Content-Type': 'application/json'
-    }
-  });
+    return axios.post('https://www.googleapis.com/calendar/v3/calendars/primary/events/import', {
+        headers: {
+        'Authorization': 'Bearer ' + accessToken,
+        'Content-Type': 'application/json'
+        }
+    });
 }
 
 export function getPrivateCalendarEvents(accessToken) {
-  return axios.get('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
-    headers: {
-      'Authorization': 'Bearer ' + accessToken,
-      'Content-Type': 'application/json'
-    },
-    params: {
-      timeMin: new Date().toISOString()
-    }
-  });
+    return axios.get(`https://www.googleapis.com/calendar/v3/calendars/primary/events/${Event.eventId}`, {
+        headers: {
+        'Authorization': 'Bearer ' + accessToken,
+        'Content-Type': 'application/json'
+        },
+        params: {
+        timeMin: new Date().toISOString()
+        }
+    });
 }
 
 export function getPublicCalendarEvents(event, accessToken) {
-  const { calendarId, eventId } = event;
-  return axios.get(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/${eventId}`, {
-    headers: {
-      'Authorization': 'Bearer ' + accessToken,
-      'Content-Type': 'application/json'
-    }
-  });
+    const { calendarId, eventId } = event;
+    return axios.get(`https://www.googleapis.com/calendar/v3/calendars/${Calendar.calendarId}/events/${Event.eventId}`, {
+        headers: {
+        'Authorization': 'Bearer ' + accessToken,
+        'Content-Type': 'application/json'
+        }
+    });
 }
