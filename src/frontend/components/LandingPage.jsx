cf138b83936a460e6802cc46df468ab56982b803
Alejandro B <alejandrojb@g.ucla.edu>
2023-03-06 20:47:57 -0800

    added landing page

--- src/frontend/components/LandingPage.jsx
+++ src/frontend/components/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 // This page will introduce our web app and give an overview of the features. 
 // It will also contain buttons for login and signup page which will redirect the user.
-function LandingPage() {
+const LandingPage = () =>  {
   return (
     <div>LandingPage</div>
   )
