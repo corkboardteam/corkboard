65cb49ddd462326f7afd7f4e87b233357806f204
heliona203 <alex.su8735@gmail.com>
2023-03-17 15:28:23 -0700

    Re-add Google Login to login page

--- src/frontend/components/Login.jsx
+++ src/frontend/components/Login.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect } from 'react';
-import { Container, Button, TextField, FormControlLabel, Checkbox, Box, Grid, Typography} from '@mui/material';
+import { Container, Button, TextField, FormControlLabel, Checkbox, Box, Grid, Typography } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom'
 import { GoogleButton } from 'react-google-button'
 import { UserAuth } from '../../backend/authContext';
+import Divider from '@mui/material/Divider';
 
-const Login =() => {
+const Login = () => {
   const { currentUser, login, googleLogin } = UserAuth();
-	const navigate = useNavigate();
-  
+  const navigate = useNavigate();
+
   useEffect(() => {
     if (currentUser != null) {
       navigate('/Fridge');
-    } 
+    }
     console.log(currentUser)
-  },[currentUser, navigate]);
+  }, [currentUser, navigate]);
 
 
   const handleLogin = async (e) => {
@@ -25,7 +26,7 @@ const Login =() => {
     navigate('/Fridge');
 
     console.log(email, password);
-    
+
   };
 
   const handleGoogleLogin = async (e) => {
@@ -35,67 +36,71 @@ const Login =() => {
 
   return (
     <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-    <Box maxWidth="50vw" sx={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Grid sx={{ mt: 4 }}>
-        <Typography component="h1" fontSize="2.8em" fontWeight='600'>
-        Log in
+      <Box maxWidth="50vw" sx={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Grid sx={{ mt: 4 }}>
+          <Typography component="h1" fontSize="2.8em" fontWeight='600'>
+            Log in
           </Typography>
-      </Grid>
-      <Container sx={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-        Don't have an account?
-        <Box sx={{ marginLeft: '5px' }}>
-        <Link to="/Signup" href="#" variant="body2">
-          {"Sign up"}
-        </Link>
-        </Box>
-      </Container>
-      <Grid sx={{ mt: 4 }}>
-        <Box component="form" noValidate onSubmit={handleLogin} sx={{ mt: 1, fontSize: "1em", fontWeight: "bold" }}>
-          Email
-          <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Enter your email"
-          name="email"
-          autoComplete="email"
-          autoFocus
-          />
-          Password
-          <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Create a password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          />
-          <Box sx={{ mt: 1, fontSize: "1em", fontWeight: "normal", display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}> 
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Link to="/ForgotPassword" href="#" variant="body2">
-              {"Forgot your password?"}
+        </Grid>
+        <Container sx={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+          Don't have an account?
+          <Box sx={{ marginLeft: '5px' }}>
+            <Link to="/Signup" href="#" variant="body2">
+              {"Sign up"}
             </Link>
           </Box>
-          <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 4 }}
-          >
-            Log in
-          </Button>
+        </Container>
+        <Box sx={{ display: "flex", justifyContent: "center", marginTop: '20px' }}>
+          <GoogleButton onClick={handleGoogleLogin} />
         </Box>
-        {/* <Box sx={{ display: "flex", justifyContent: "center"}}>
+        <Divider sx={{ marginTop: '20px' }}>OR</Divider>
+        <Grid sx={{ mt: 4 }}>
+          <Box component="form" noValidate onSubmit={handleLogin} sx={{ mt: 1, fontSize: "1em", fontWeight: "bold" }}>
+            Email
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Enter your email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            Password
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Create a password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Box sx={{ mt: 1, fontSize: "1em", fontWeight: "normal", display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Link to="/ForgotPassword" href="#" variant="body2">
+                {"Forgot your password?"}
+              </Link>
+            </Box>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 4 }}
+            >
+              Log in
+            </Button>
+          </Box>
+          {/* <Box sx={{ display: "flex", justifyContent: "center"}}>
           <GoogleButton onClick={handleGoogleLogin} />
         </Box> */}
-      </Grid>
-    </Box>
+        </Grid>
+      </Box>
     </Container>
   )
 }
